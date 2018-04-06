ae2ac16818606b6a78c7207d86cafd992b982436
Tomosugi Tasaka <tuvy.ano@gmail.com>
2018-04-06 19:03:25 +0900

    databinding

--- app/src/main/java/com/tuvy/tomosugi/sampleapp/MainActivity.kt
+++ app/src/main/java/com/tuvy/tomosugi/sampleapp/MainActivity.kt
@@ -2,11 +2,16 @@ package com.tuvy.tomosugi.sampleapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.tuvy.tomosugi.sampleapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
