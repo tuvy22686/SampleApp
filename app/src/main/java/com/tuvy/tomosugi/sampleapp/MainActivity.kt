befdbfe5f9ad17b9e2d038838677d3b4d42d62dd
Tomosugi Tasaka <tuvy.ano@gmail.com>
2018-04-06 19:09:49 +0900

    add guide about activity of life cycle

--- app/src/main/java/com/tuvy/tomosugi/sampleapp/MainActivity.kt
+++ app/src/main/java/com/tuvy/tomosugi/sampleapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.tuvy.tomosugi.sampleapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.tuvy.tomosugi.sampleapp.databinding.ActivityMainBinding
 
@@ -12,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("LifeCycle", "onCreate")
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
@@ -19,5 +21,30 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "ボタンが押されました", Toast.LENGTH_SHORT)
                     .show()
         }
+    }
+
+    override fun onStart() {
+        Log.d("LifeCycle", "onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d("LifeCycle", "onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d("LifeCycle", "onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d("LifeCycle", "onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d("LifeCycle", "onDestroy")
+        super.onDestroy()
     }
 }
